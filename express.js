33c0a5642daf41c5687b5fc75d2d33af7c1e17a2
Phùng Đức Nhật <ducnhatphung@gmail.com>
2016-11-18 21:25:00 +0700

    fix expressjs

--- express.js
+++ express.js
@@ -4,13 +4,14 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 const server = express()
 const router = express.Router()
 
 const layout = fs.readFileSync('dist/index.html', 'utf8')
 
+server.use(express.static(`${__dirname}/dist`));
 
 server.get('/', (request, response) => {
   response.send(layout)
